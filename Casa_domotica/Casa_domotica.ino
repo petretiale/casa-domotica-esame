40ad8c8c1faaca32231b4a844b19b52df83548a9
Alessandro Petreti <petretiale@gmail.com>
2021-05-31 21:46:46 +0200

    aggiunto cancello

--- Casa_domotica/Casa_domotica.ino
+++ Casa_domotica/Casa_domotica.ino
@@ -2,6 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <RFID.h>
 #include <SimpleDHT.h>
+#include <Servo.h>
 
 #define NOTA1 262
 #define NOTA2 280
@@ -9,48 +10,64 @@
 #define ALLARME_INSERITO 1
 #define ALLARME_ATTIVO 2
 #define INDIRIZZO_LCD 0x27
+#define SOGLIA 23 - 1
 
-int pirPin = 7; // Input for HC-S501
-int pinBuzzer = 2;
-int pinSda = 53;  
-int pinReset = 5;
-int pinLuceBlu = 3;
-int pinLuceVerde = 9;
-int pinLuceRossa = 6;
-int pinDHT11 = 4;
-int pinVentola = 10;
-int ledPin[] = {36,38,40,42,44,46};
-int pulsantePin[] = {22,24,26,28,30,32};
-int pinFiamma = 13;
+int pirPin = 7; // Pin sensore PIR
+int pinBuzzer = 2; // Pin buzzer
+int pinSda = 53;  //Pin lettore tessera
+int pinReset = 5; // Pin reset lettore tessera
+int pinLuceBlu = 3;  // Pin led rgb blu
+int pinLuceVerde = 9; // Pin led rgb verde
+int pinLuceRossa = 6; // Pin led rgb rossa
+int pinDHT11 = 4; // Pin sensore di umidità e temperatura
+int pinVentola = 10;  // Pin motore dc
+int ledPin[] = {36,38,40,42,44,46}; // Pin led
+int pulsantePin[] = {22,24,26,28,30,32}; // Pin pulsanti
+int pinFiamma = 13; // Pin sensore di fiamma
+int pinServo = 11; // Pin servo cancello
 
-int statoSensoreFiamma = 0;
-int statoLed[] = {LOW,LOW,LOW,LOW,LOW,LOW};
-int statoPulsante[] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
-int ultimaLetturaPulsante[] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
-int attesaDebouncePulsante= 50;
-unsigned long ultimoTempoDebouncePulsante = 0;
+// stato iniziale sensore di fiamma = 0
+int statoSensoreFiamma = 0; 
+// vettore per lo stato dei led
+int statoLed[] = {LOW,LOW,LOW,LOW,LOW,LOW}; 
+// vettore per lo stato dei pulsanti
+int statoPulsante[] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH}; 
+// vettore per l'ultimo stato dei pulsanti
+int ultimaLetturaPulsante[] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH, HIGH};
 int stato_allarme;
 int stato_lcd;
 int pirValue;
-RFID rc522(pinSda, pinReset);
-unsigned long ultimaLetturaTag = 0;
+int stato_cancello = 0; // stato iniziale chiuso
+int temp_attuale = 0; // stato iniziale temperatura
+int umi_attuale = 0; // stato iniziale umidità
+int attesaDebouncePulsante= 50;
 int antirimbalzoTag = 1000;
-LiquidCrystal_I2C lcd(INDIRIZZO_LCD,16,2);
-SimpleDHT11 dht11;
 int tempo_aggiornamento_lcd = 2000;
+unsigned long ultimoTempoDebouncePulsante = 0;
 unsigned long ultimaScritturalcd = 0;
-int temp_attuale = 0;
-int umi_attuale = 0;
+unsigned long ultimaLetturaTag = 0;
+RFID rc522(pinSda, pinReset); // lettore RFID
+LiquidCrystal_I2C lcd(INDIRIZZO_LCD,16,2); // LCD
+SimpleDHT11 dht11; // Sensore umidità e temperatura
+Servo cancello; // servo motore cancello
 
 
 
 /** Funzioni **/
-void suona_allarme();
-String leggi_tag(RFID& lettore);
+// funzione per il suono dell'allarme
+void suona_allarme(); 
+// funzione per la lettura Tag
+String leggi_tag(RFID& lettore); 
+// funzione per il controllo dei colori del led rgb
 void led_colore(int rosso, int verde, int blu);
-void messaggio_lcd(String messaggio);
+// funzione per stampare messaggi sul display LCD
+void messaggio_lcd(String messaggio); 
+// funzione per convertire gli stati dell'allarme in messaggi
 String converti_stato_allarme(int stato_allarme);
+// funzione per l'accensione e lo spegnimento dei led
 void accendiSpegniLed(int statoLed[],int ledPin[], int i);
+// funzione per aprire e chiudere il cancello
+void apriChiudiCancello();
 
 void setup() {
   Serial.begin(9600);
@@ -60,24 +77,26 @@ void setup() {
   lcd.init();
   lcd.backlight();
   
-  stato_allarme = ALLARME_SPENTO;
- 
+  stato_allarme = ALLARME_SPENTO; // stato iniziale 
+
   pinMode(pirPin, INPUT);
   pinMode(pinBuzzer,OUTPUT);
   pinMode(pinLuceRossa, OUTPUT);
   pinMode(pinLuceVerde, OUTPUT);
   pinMode(pinLuceBlu, OUTPUT);
   pinMode(pinFiamma, INPUT);
-  for(int i = 0; i < 6; i++){
+  // inizializzazione dei led e dei pulsanti
+  for(int i = 0; i < 6; i++){ 
     pinMode(ledPin[i],OUTPUT);
     digitalWrite(ledPin[i],LOW);
+    pinMode(pulsantePin[i], INPUT_PULLUP);
   }
-  for(int j = 0; j < 6; j++){
-    pinMode(pulsantePin[j], INPUT_PULLUP);
-  }
+  // inizializzazione cancello e pulsante
+  cancello.attach(pinServo);
+  pinMode(pulsantePin[6], INPUT_PULLUP);
     
   messaggio_lcd("caricamento...");
-  delay(60000);
+  delay(60000); // attesa per inizializzare PIR
   Serial.println("Pronto!\n");
 }
 
@@ -85,7 +104,7 @@ void loop() {
   byte temperatura = 0;
   byte umidita = 0;
   byte data[40] = {0};
-   
+  // gestione dei comandi provenienti da ESP32
   if(Serial1.available() > 0) {
     int comando = Serial1.parseInt();
     Serial1.println(1); // invio 1 (ok) ad esp32
@@ -106,13 +125,19 @@ void loop() {
         break;
       case 5:
         accendiSpegniLed( statoLed ,ledPin , 4);
+        break;
+      case 6:
+        accendiSpegniLed(statoLed, ledPin, 5);
         break;   
-      case 6:   //allarme inserito
+      case 7:   //allarme inserito
         if(stato_allarme == ALLARME_SPENTO){
           stato_allarme = ALLARME_INSERITO;
         }else{
           stato_allarme = ALLARME_SPENTO; 
         }
+        break;
+      case 8:
+        apriChiudiCancello();
         break; 
       case 10:
         Serial1.println(temp_attuale);
@@ -122,8 +147,8 @@ void loop() {
         break;
     }
   }
-  
-  for(int i = 0; i < 6; i++){
+  //  lettura con antirimbalzo pulsanti
+  for(int i = 0; i < 7; i++){
     
     int lettura = digitalRead(pulsantePin[i]);
     if(lettura != ultimaLetturaPulsante[i]){            
@@ -132,30 +157,35 @@ void loop() {
   
     if((millis() - ultimoTempoDebouncePulsante) > attesaDebouncePulsante){   
       if(lettura != statoPulsante[i] and lettura == LOW){     
-        statoLed[i] = !statoLed[i];                            
-        digitalWrite(ledPin[i], statoLed[i]);                   
+        if(i < 6) { // controllo i led solo se i è tra i primi 6 (0..5)
+          statoLed[i] = !statoLed[i];                            
+          digitalWrite(ledPin[i], statoLed[i]);          
+        } else {
+          apriChiudiCancello();
+        }
       }
       statoPulsante[i] = lettura;                               
     }
-  
-    ultimaLetturaPulsante[i] = lettura;      
+    ultimaLetturaPulsante[i] = lettura;     
   }
   
+  // lettura di temperatura e umidità dal sensore DHT11
   if (!dht11.read(pinDHT11, &temperatura, &umidita, data)) {
     temp_attuale =(int)temperatura;
     umi_attuale =(int)umidita;
   }
-  
-  if(temp_attuale > 20){
+   // controllo impianto di aria 
+  if(temp_attuale > SOGLIA){
    analogWrite(pinVentola, 255);
   }else{
     analogWrite(pinVentola, 0);
   }
-
+   // antirimbalzo per il display LCD
   if((millis()- ultimaScritturalcd) > tempo_aggiornamento_lcd){
     ultimaScritturalcd = millis();
     switch(stato_lcd){
-      case 0:{
+      //scrittura di temperatura e umidità sul display LCD
+      case 0:{ 
         String lcd_temp = (String)"temperatura  " + temp_attuale + "C";
         String lcd_umi = (String)"umidita  "+ umi_attuale + "%";
         Serial.println(lcd_temp);
@@ -168,8 +198,8 @@ void loop() {
         stato_lcd = 1;
         break;
       }
-      case 1:
-        
+      // scrittura dello stato di allarme sul display LCD
+      case 1:   
         String lcd_allarme = converti_stato_allarme( stato_allarme);
         Serial.println(lcd_allarme);
         lcd.clear();
@@ -179,13 +209,15 @@ void loop() {
         break;
     }
   }
+  // lettura Rfid con antirimbalzo 
   if (rc522.isCard()){
     if((millis()- ultimaLetturaTag) > antirimbalzoTag){
       String codiceCarta = leggi_tag(rc522);
       Serial.println(codiceCarta);
       ultimaLetturaTag = millis();
+      // se il codice della carta viene riconosciuto 
       if(codiceCarta == "B29A872788"){
-        Serial.println("riconosciuto");
+        // cambio stato allarme
         switch(stato_allarme){
           case ALLARME_SPENTO:
             stato_allarme = ALLARME_INSERITO;
@@ -199,30 +231,31 @@ void loop() {
     }
   }
 
-  if(stato_allarme == ALLARME_SPENTO){
-    led_colore(0,0,0);
+  if(stato_allarme == ALLARME_SPENTO){ // se lo stato allarme è spento
+    led_colore(0,0,0); // spengo il led
   }
   
-  if(stato_allarme == ALLARME_INSERITO){
-    led_colore(255,0,0);
-    pirValue = digitalRead(pirPin);
-    Serial.println(pirValue);
-    if(pirValue == HIGH){
-      stato_allarme = ALLARME_ATTIVO;
+  if(stato_allarme == ALLARME_INSERITO){ // se lo stato allarme è inserito
+    led_colore(255,0,0); // accendo il led
+    pirValue = digitalRead(pirPin); // leggo lo stato del sensore Pir
+    Serial.println(pirValue); // stampo su seriale lo stato del sensore Pir
+    if(pirValue == HIGH){ // se il lo stato del sensore Pir è alto
+      stato_allarme = ALLARME_ATTIVO; // lo stato del allarme passa ad attivo
       
     }
   }
 
-  if(stato_allarme == ALLARME_ATTIVO){
-    led_colore(255,0,0);
-    delay(200);
-    led_colore(0,0,0);
-   // suona_allarme();
+  if(stato_allarme == ALLARME_ATTIVO){ // se lo stato allarme è attivo
+    led_colore(255,0,0); //accendo il led 
+    delay(200); // aspetta 2 ms
+    led_colore(0,0,0); //spengo il led 
+    suona_allarme(); // richiamo la funzione suona allarme
   }
-    statoSensoreFiamma = digitalRead(pinFiamma);
-    if(statoSensoreFiamma == HIGH){
-      stato_allarme = ALLARME_ATTIVO;
-    }
+  
+  statoSensoreFiamma = digitalRead(pinFiamma); // leggo lo stato del sensore di fiamma 
+  if(statoSensoreFiamma == HIGH){ // se lo stato del sensore di fiamma è alto 
+    stato_allarme = ALLARME_ATTIVO; // lo stato del allarme passa ad attivo
+  }
 }
 
 
@@ -280,5 +313,19 @@ void accendiSpegniLed(int statoLed[], int ledPin[], int i){
   }else{
     digitalWrite(ledPin[i], HIGH);
     statoLed[i] = HIGH;
+  }
+}
+
+void apriChiudiCancello() {
+  if(stato_cancello == 1) {
+    cancello.write(71);
+    delay(4000);
+    cancello.write(70);
+    stato_cancello = 0;
+  } else {
+    cancello.write(68);
+    delay(4000);
+    cancello.write(70);
+    stato_cancello = 1;
   }
 }
